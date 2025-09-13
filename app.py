57f714007e166e35fc7e13bbc3f4ce1efbd754ec
Victor Kip <vicklan132@gmail.com>
2025-09-13 11:46:08 +0300

    feat: add registration page
    add input field for email and password,put a placeholder for google oauth
    
    issue #2

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #routes
+#landing page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+#register page
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
