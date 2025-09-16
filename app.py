85ae6be11aa865d991b21ecdc673201cf4c8bd1a
Victor Kip <vicklan132@gmail.com>
2025-09-16 12:07:58 +0300

    feat : update registration backend
    add password checking,adding user to the database and flash messages to show errrors
    
    issue #2

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template,redirect,session,request
-from flask_sqlalchemy import SQLALchemy
-from models import User,Product,PriceHistory,Forecast,Recommendation
-
+from flask import Flask, render_template,redirect,session,request,url_for,flash
+from extensions import db
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "my_secret_key"
 
 #configure sqlalchemy
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///genecomm.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLALchemy(app)
+
+#initialize db with app
+db.init_app(app)
+from my_db_models import User, Product, PriceHistory, Forecast, Recommendation
+
+
 
 
 
@@ -18,9 +22,28 @@ db = SQLALchemy(app)
 def index():
     return render_template("index.html")
 
-#route to register page
-@app.route('/register')
+#route to registration page and process registration details
+@app.route('/register', methods = ["GET","POST"])
 def register():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        if(password == confirm_password):
+            user = User.query.filter_by(email = email).first()
+            if user:
+                flash("User already exists!", "warning")
+                return redirect(url_for("register"))
+                
+            else:
+                new_user = User(email = email)
+                new_user.set_password(password)
+                db.session.add(new_user)
+                db.session.commit()
+        else:
+            flash("Unmatched!", "danger")
+            return redirect(url_for("register"))          
+
     return render_template("register.html")
 
 # route to login page and process login request
@@ -33,4 +56,6 @@ def login():
  
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
